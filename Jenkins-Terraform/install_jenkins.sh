3284034995ca199b4ce106f501d29eb3cad53b61
gabin-sime <gabin75@yahoo.de>
2024-03-23 02:20:28 -0500

    Jenkins-terraform file modified

--- Jenkins-Terraform/install_jenkins.sh
+++ Jenkins-Terraform/install_jenkins.sh
@@ -27,13 +27,6 @@ sudo chmod 777 /var/run/docker.sock
 # Install sonarqube
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
-# install trivy
-sudo apt-get install wget apt-transport-https gnupg lsb-release -y
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt update
-sudo apt install trivy -y
-
 #install Kubectl
 sudo apt update -y
 sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
@@ -41,10 +34,20 @@ sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 kubectl version --client
 
+
+#install terraform
+sudo apt update -y
+sudo apt install gnupg -y
+sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform -y
+
 #install Aws cli 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
-#Install Splunk
+
